2fd3b4e00cd4df9db7b4a76d3d872d053b80f101
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-04-22 11:30:10 -0300

    Started project

---
+++ src/main/java/com/jvmausa/biblioteca/BibliotecaBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.jvmausa.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBackendChallengeApplication.class, args);
+	}
+
+}
