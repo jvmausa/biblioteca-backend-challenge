a9265df1411d43a78d1d409db031f4acb46e8d1f
João Victor Mausa Rocha do Canto <jvmausa.it@gmail.com>
2022-04-27 14:35:19 -0300

    added try/catch for deleteById exception

--- src/main/java/com/jvmausa/biblioteca/domain/service/CadastroLivroService.java
+++ src/main/java/com/jvmausa/biblioteca/domain/service/CadastroLivroService.java
@@ -3,6 +3,7 @@ package com.jvmausa.biblioteca.domain.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jvmausa.biblioteca.domain.exceptionhandler.LivroNaoEncontradoException;
@@ -31,7 +32,13 @@ public class CadastroLivroService {
 	
 	@Transactional
 	public void excluirLivro(Long id) {
-		livroRepository.deleteById(id);
+		
+		try {
+			livroRepository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new LivroNaoEncontradoException(id);
+		}
 		
 	}
 	
